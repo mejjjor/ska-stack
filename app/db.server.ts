6460eaf19c48ba52e49815484361a58efc3742ce
erik <erik.aouizerate@mintset.io>
2022-09-05 00:51:17 +0200

    to s with prisma

--- app/db.server.ts
+++ app/db.server.ts
@@ -7,10 +7,18 @@ declare global {
   var __db__: PrismaClient;
 }
 
-if (!global.__db__) {
-  global.__db__ = getClient();
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+// in production we'll have a single connection to the DB.
+if (process.env.NODE_ENV === "production") {
+  prisma = getClient();
+} else {
+  if (!global.__db__) {
+    global.__db__ = getClient();
+  }
+  prisma = global.__db__;
 }
-prisma = global.__db__;
 
 function getClient() {
   const { POSTGRESQL_ADDON_URI } = process.env;
